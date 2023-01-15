bd4076c2a3c2956255dde4c4a56e55871ea042ac
Wes <wmont012@ucr.edu>
2023-01-15 00:44:03 -0800

    Merge successful

--- rosehack2023Game/Assets/Scripts/CrateBreaker.cs
+++ rosehack2023Game/Assets/Scripts/CrateBreaker.cs
@@ -7,6 +7,7 @@ public class CrateBreaker : MonoBehaviour
 
     [SerializeField] CrateHandler ch;
     [SerializeField] int crateID;
+    [SerializeField] ParticleSystem splinterFX;
 
     bool canBreak;
 
@@ -19,6 +20,10 @@ public class CrateBreaker : MonoBehaviour
     {
         if (col.gameObject.tag == "Ground" && canBreak)
         {
+            // Debug.Log("TOUCHING GrOUND AAAAAAAAAAAAAAAAAAAAAA");
+            ParticleSystem temp = Instantiate(splinterFX, transform.position, Quaternion.identity);
+            temp.Play();
+            ch.breakCrate(gameObject);
             canBreak = false;
             FindObjectOfType<SFXPlayer>().PlayBreakCrateSFX();
             ch.breakCrate(gameObject);
