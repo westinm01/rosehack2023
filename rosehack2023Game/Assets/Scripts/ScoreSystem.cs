17d9931bb6317d20bd1c205073c384d7ba1f6a1b
NikOhNo <80977626+NikOhNo@users.noreply.github.com>
2023-01-14 20:11:37 -0800

    Basic Score System

---
+++ rosehack2023Game/Assets/Scripts/ScoreSystem.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class ScoreSystem : MonoBehaviour
+{
+    const bool LEFT_PLAYER = true;
+    const bool RIGHT_PLAYER = false;
+
+    [SerializeField]
+    TMP_Text leftPlayerScoreText;
+    [SerializeField]
+    TMP_Text rightPlayerScoreText;
+
+    private int leftScore = 0;
+    private int rightScore = 0;
+
+    public void AddScore(bool team, int amount)
+    {
+        if (team == LEFT_PLAYER)
+        {
+            leftScore += amount;
+            UpdateText(leftPlayerScoreText, leftScore);
+        }
+        else
+        {
+            rightScore += amount;
+            UpdateText(rightPlayerScoreText, rightScore);
+        }
+    }
+
+    private void UpdateText(TMP_Text textField, int score)
+    {
+        textField.text = score.ToString();
+    }
+}
